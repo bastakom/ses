8e75c77c3af891d0aad2dddd39b11589308c69e7
BK <philip@bastakompisar.se>
2023-04-23 23:44:44 +0200

    testing axios

--- graphql/PostTypes/news.ts
+++ graphql/PostTypes/news.ts
@@ -1,11 +1,13 @@
+import axios from 'axios'
+
 export const getNyheter = async (locale) => {
   const correctLocale = locale === 'sv' ? [] : locale
-  const data = await fetch(
-    `${process.env.NEXT_PUBLIC_WP_URL}${correctLocale}/wp-json/wp/v2/nyheter?cache=${Math.random()}`,
-    { headers: { 'cache-control': 'no-cache' } }
-  )
-  const response = await data.json()
+  const url = `${process.env.NEXT_PUBLIC_WP_URL}${correctLocale}/wp-json/wp/v2/nyheter`
 
+  const response = await axios.get(url, {
+    params: { cache: Math.random() },
+    headers: { 'cache-control': 'no-cache' }
+  })
 
-  return response
+  return response.data
 }
