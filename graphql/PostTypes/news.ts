a4cbb63c5429235a4abdca42085989cf1945ac37
BK <philip@bastakompisar.se>
2023-04-24 00:28:55 +0200

    last try

--- graphql/PostTypes/news.ts
+++ graphql/PostTypes/news.ts
@@ -2,11 +2,15 @@ import axios from 'axios'
 
 export const getNyheter = async (locale) => {
   const correctLocale = locale === 'sv' ? [] : locale
-  const url = `${process.env.NEXT_PUBLIC_WP_URL}${correctLocale}/wp-json/wp/v2/nyheter`
+  const url = `${
+    process.env.NEXT_PUBLIC_WP_URL
+  }${correctLocale}/wp-json/wp/v2/nyheter?_=${Date.now()}`
 
   const response = await axios.get(url, {
-    params: { _cache: Date.now() },
-    headers: { 'Cache-Control': 'no-cache' }
+    headers: {
+      'Cache-Control': 'no-cache',
+      Pragma: 'no-cache'
+    }
   })
 
   return response.data
