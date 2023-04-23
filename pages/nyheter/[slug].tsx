52711296e0eb4e9edda8fe26106208efafca77fb
BK <philip@bastakompisar.se>
2023-04-23 22:33:08 +0200

    trying to fix  caching

--- pages/nyheter/[slug].tsx
+++ pages/nyheter/[slug].tsx
@@ -46,7 +46,7 @@ export const getStaticProps = async ({ params, locale }) => {
 
   const data = await fetch(
     `${process.env.NEXT_PUBLIC_WP_URL}${correctLocale}/wp-json/wp/v2/nyheter?slug=${params.slug}`,
-    { next: { revalidate: 10 } }
+    { headers: { 'cache-control': 'no-cache' } }
   )
   const response = await data.json()
   return {
