62a746b22c328073957fa9f9d9481194d379fec2
BK <philip@bastakompisar.se>
2023-04-25 14:35:34 +0200

    google font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* Add any necessary meta tags, stylesheets, or scripts here */}
+          {/* link your fonts here */}
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;1,400&family=Montserrat:wght@300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
         </Head>
+
         <>
           <Main />
           <NextScript />
         </>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
