dad9e89e3e4fd7614200134626ab1bf7bb808ae4
BK <philip@bastakompisar.se>
2023-05-04 13:04:16 +0200

    cookies test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,16 +5,25 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* link your fonts here */}
-          <link
-            rel="preconnect"
-            href="https://fonts.googleapis.com"
-          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;1,400&family=Montserrat:wght@300;400;500;600&display=swap"
             rel="stylesheet"
           />
+          <script
+            id="Cookiebot"
+            src="https://consent.cookiebot.com/uc.js"
+            data-cbid="54a96287-0a8e-4874-9434-a6d52758b612"
+            data-blockingmode="auto"
+            type="text/javascript"
+          ></script>
+          <script
+            id="CookieDeclaration"
+            src="https://consent.cookiebot.com/54a96287-0a8e-4874-9434-a6d52758b612/cd.js"
+            type="text/javascript"
+            async
+          ></script>
         </Head>
 
         <>
