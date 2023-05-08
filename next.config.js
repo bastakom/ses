cf353b20ac4609aa6dc9c2468b4d0210c80a0f73
BK <philip@bastakompisar.se>
2023-05-08 14:55:44 +0200

    head next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: [
+    {
+      key: 'Cache-Control',
+      value: 'public, max-age=180, s-maxage=180, stale-while-revalidate=180'
+    }
+  ],
   i18n: {
     locales: ['sv', 'en'],
     defaultLocale: 'sv',
