b98cd615d0e7d20e4eafc057c8098134a629ebee
BK <philip@bastakompisar.se>
2023-05-08 16:25:22 +0200

    immutable

--- next.config.js
+++ next.config.js
@@ -13,7 +13,20 @@ module.exports = {
     domains: [process.env.WP_IMAGES_URL],
     loader: 'default',
     deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [16, 32, 48, 64, 96]
+    imageSizes: [16, 32, 48, 64, 96],
+    headers: async () => {
+      return [
+        {
+          source: '/_next/(.*)',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=31536000, immutable'
+            }
+          ]
+        }
+      ]
+    }
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
