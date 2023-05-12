6fd01a2406db9d6fc690d833957c0ef40b64070f
BK <philip@bastakompisar.se>
2023-05-12 09:50:54 +0200

    modify 500 erro page

--- pages/500.js
+++ pages/500.js
@@ -2,8 +2,14 @@ import { getPageStaticProps } from '../utils/getPageStaticProps'
 
 const Error = () => {
   return (
-    <div className="bg-white">
-      <h1 style={{ color: 'black' }}>Page Not Found 500</h1>
+    <div className="h-screen">
+      <div className="bg-white h-5/6 flex flex-col items-center justify-center">
+        <h1 style={{ color: 'black' }}>SERVER ERROR 500</h1>
+        <p>Sidan du sökt på verkar ej finns 🥲</p>
+        <button className="button mt-10">
+          <a href="/">Tillbaka till startsidan</a>
+        </button>
+      </div>
     </div>
   )
 }
