848f882ea1a87fbba85206e81de9c6693369e6e4
BK <philip@bastakompisar.se>
2023-12-07 09:30:22 +0100

    footer changes

--- components/Template/Footer/Footer.tsx
+++ components/Template/Footer/Footer.tsx
@@ -73,9 +73,7 @@ const Footer = ({ options }) => {
             <div
               className={`${styles.footer__info} flex justify-center flex-col items-center`}
             >
-              <h2 className="pb-2 uppercase mb-2">
-                {locale === 'sv' ? 'Operativ chef' : 'Chief Operating Officer'}
-              </h2>
+              <h2 className="pb-2 uppercase mb-2">PRESIDENT</h2>
               <div className={`${styles.contanct__info} flex gap-2`}>
                 <h3>{email}</h3>
                 <h3>{name}</h3>
@@ -88,7 +86,9 @@ const Footer = ({ options }) => {
             <div
               className={`${styles.footer__info} flex justify-center flex-col items-center `}
             >
-              <h2 className="pb-2">PRESIDENT</h2>
+              <h2 className="pb-2 uppercase mb-2">
+                {locale === 'sv' ? 'Operativ chef' : 'Chief Operating Officer'}
+              </h2>
               <div className={`${styles.contanct__info} flex gap-2`}>
                 <h3>{presidentEmail}</h3>
                 <h3>{presidentName}</h3>
