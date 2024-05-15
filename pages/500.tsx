5cee00f10e2ea82bf752d0926615cb939ef4992c
BK <philip@bastakompisar.se>
2024-05-15 10:36:08 +0200

    får inte använda bilden av ngn anlendning

--- pages/500.tsx
+++ pages/500.tsx
@@ -5,7 +5,6 @@ const Error = () => {
     <div className="h-screen">
       <div className="bg-white h-5/6 flex flex-col items-center justify-center">
         <h1 style={{ color: 'black' }}>SERVER ERROR 500</h1>
-        <img src={`https://ses.admin-login.se/wp-content/uploads/2024/05/clueless.jpeg`} />
         <p>Sidan du sökt på verkar ej finns</p>
         <button className="button mt-10">
           <a href="/">Tillbaka till startsidan</a>
