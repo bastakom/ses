c60a38e0d98635a0122bd31420d8192e3c4cd689
BK <philip@bastakompisar.se>
2024-05-15 10:46:23 +0200

    fixed

--- pages/500.tsx
+++ pages/500.tsx
@@ -4,8 +4,8 @@ const Error = () => {
   return (
     <div className="h-screen">
       <div className="bg-white h-5/6 flex flex-col items-center justify-center">
-        <h1 style={{ color: 'black' }}>SERVER ERROR 500</h1>
-        <p>Sidan du sökt på verkar ej finns</p>
+        <h1 style={{ color: 'black' }}>SERVER ERROR 404</h1>
+        <p>Sidan du sökt på verkar ej finnas</p>
         <button className="button mt-10">
           <a href="/">Tillbaka till startsidan</a>
         </button>
