4934d9fc70876d399e90c02c37ed20ec34346432
BK <philip@bastakompisar.se>
2024-10-15 12:24:02 +0200

    from bk

--- components/Template/Footer/Footer.tsx
+++ components/Template/Footer/Footer.tsx
@@ -108,7 +108,7 @@ const Footer = ({ options }) => {
           )}
         </div>
       </div>
-
+      {/* Footer */}
       <div className={`flex md:justify-end items-center justify-center`}>
         <p className={`${styles.copywrite__}`}>© SES PROTECTION/SES GROUP</p>
       </div>
